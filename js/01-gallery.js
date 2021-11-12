0febaea79bcbae47b1832b04fb2b4edf005d2e4d
H.Long Nguyen <84514305+HLongNguyen0@users.noreply.github.com>
2021-11-13 00:49:35 +0200

    v1.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const imgStringHtml = galleryItems
+  .map(
+    (e, index) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href="">
+            <img
+            class="gallery__image"
+            src="${galleryItems[index].preview}"
+            data-source="large-image.jpg"
+            alt="Image description"
+            />
+        </a>
+    </div>
+    `,
+  )
+  .join('');
+
+gallery.innerHTML = imgStringHtml;
+
+gallery.addEventListener('click', showModal);
+
+function showModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.src}" width="800" height="600">
+`);
+  instance.show();
+}
