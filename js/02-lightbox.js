5cfbde0236a0c54c05b1635c860bb2d563da5d69
H.Long Nguyen <84514305+HLongNguyen0@users.noreply.github.com>
2021-11-13 18:04:20 +0200

    v2.4

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,14 +7,50 @@ const gallery = document.querySelector('.gallery');
 
 const imgStringHtml = galleryItems
   .map(
-    (e, index) => `
-        <a class="gallery__item" href="${galleryItems[index].preview}">
-            <img class="gallery__image" src="${galleryItems[index].preview}" alt="Image description" />
+    ({ preview, original, description }) => `
+    <li>
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image lazyload"
+            loading="lazy"
+            data-src="${preview}" 
+            alt="${description}" />
         </a>
+    </li>
     `,
   )
   .join('');
 
-gallery.innerHTML = imgStringHtml;
+gallery.insertAdjacentHTML('beforeend', imgStringHtml);
 
-const modal = new SimpleLightbox('.gallery a');
+const modal = new SimpleLightbox('.gallery__link', {
+  captionData: 'alt',
+  captionDelay: 250,
+});
+
+const lazyImgs = document.querySelectorAll('img[loading="lazy"]');
+
+if ('loading' in HTMLImageElement.prototype) {
+  lazyImgs.forEach(img => (img.src = img.dataset.src));
+  lazyImgsListener();
+} else {
+  const script = document.createElement('script');
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+  script.integrity =
+    'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
+  script.crossOrigin = 'anonymous';
+  document.body.appendChild(script);
+  lazyImgsListener();
+}
+
+function lazyImgsListener() {
+  lazyImgs.forEach(image =>
+    image.addEventListener(
+      'load',
+      event => {
+        event.target.classList.add('appear');
+      },
+      { once: true },
+    ),
+  );
+}
