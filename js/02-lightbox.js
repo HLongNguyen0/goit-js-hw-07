d1f51a07de1b819726475efd0669a3d69efbb51c
H.Long Nguyen <84514305+HLongNguyen0@users.noreply.github.com>
2021-11-13 11:56:59 +0200

    v2.1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const imgStringHtml = galleryItems
+  .map(
+    (e, index) => `
+        <a class="gallery__item" href="${galleryItems[index].preview}">
+            <img class="gallery__image" src="${galleryItems[index].preview}" alt="Image description" />
+        </a>
+    `,
+  )
+  .join('');
+
+gallery.innerHTML = imgStringHtml;
+
+gallery.addEventListener('click', showModal);
+
+function showModal(event) {
+  event.preventDefault();
+  gallery.removeEventListener('click', showModal);
+  if (event.target.nodeName != 'IMG') {
+    return;
+  }
+  const modal = new SimpleLightbox('.gallery a');
+}
